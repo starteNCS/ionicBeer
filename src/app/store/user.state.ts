b9460596d6a91747f70f36ddc58638cd0540ebe7
Philipp Honsel <philipphonsel@gmail.com>
2021-03-22 17:11:07 +0100

    Use firebase directly

--- src/app/store/user.state.ts
+++ src/app/store/user.state.ts
@@ -1,5 +1,4 @@
-import { UserService } from './../utils/services/user.service';
-import { ResponseService } from './../utils/services/response.service';
+import firebase from 'firebase';
 import { UserStateModel } from './models/user-state.model';
 import { Injectable } from "@angular/core";
 import { Action, Selector, State, StateContext } from "@ngxs/store";
@@ -23,8 +22,6 @@ export class UserState {
     }
 
     constructor(
-        private readonly responseService: ResponseService,
-        private readonly userService: UserService,
         private readonly router: Router,
         private readonly firebaseAuth: AngularFireAuth
     ) { }
@@ -35,26 +32,16 @@ export class UserState {
 
         await this.firebaseAuth
             .createUserWithEmailAndPassword(action.email, action.password)
-            .then((response: firebase.default.auth.UserCredential) => {
-                const createdUser = response.user;
-                this.responseService.handleStoreRequest(
-                    this.userService.createUser(createdUser.uid, action.createUserModel),
-                    () => new User.CreateSuccess(action.createUserModel.name),
-                    (errors) => new User.CreateFail("Du wurdest erfolgreich registriert, allerdings trat beim Speichern deines Namens ein Fehler auf. Bitte trage diesen nach.")
-                );
+            .then((response: firebase.auth.UserCredential) => {
+                firebase.auth().currentUser.updateProfile({
+                    displayName: action.createUserModel.name
+                });
                 this.router.navigateByUrl('/');
             }, (error) => {
                 context.dispatch(new User.RegisterFail("Beim Erstellen deines Kontos ist ein Fehler aufgetreten: " + error.message));
                 return;
             }).finally(() => this.patchLoadingOperations(context, -1))
         
-    }
-
-    @Action(User.CreateSuccess)
-    createUserSuccess(context: StateContext<UserStateModel>, action: User.CreateSuccess): void {
-        context.patchState({currentUser: {
-            name: action.name
-        }});
     }
 
     private patchLoadingOperations(context: StateContext<UserStateModel>, by: number): void {
